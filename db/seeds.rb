f75f99b2ab129fdfe236edcbfe1c36d11fa04718
Brian Zanti <brianzanti@gmail.com>
2019-08-22 08:49:47 -0600

    adds more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,12 @@ Artist.destroy_all
 
 journey = Artist.create(name: "Journey")
 rick = Artist.create(name: "Rick Astley")
+blink = Artist.create(name: "Blink-182")
+
 journey.songs.create!(title: "Don't Stop Believin'", length: 303, play_count: 123456)
+journey.songs.create!(title: "Wheel in the Sky", length: 673, play_count: 922)
+
 rick.songs.create!(title: "Never Gonna Give You Up", length: 253, play_count: 987654321)
+
+blink.songs.create!(title: "Miss You", length: 544, play_count: 178498)
+blink.songs.create!(title: "What's My Age Again", length: 722, play_count: 47299)

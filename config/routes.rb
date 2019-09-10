9a1aba06f5fdfcfc3ab5a8749e5368f9b0cc3e24
Meghan Stang <mstang15@gmail.com>
2019-09-10 09:54:25 -0600

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/songs', to: "songs#index"
-  get "/songs/:id", to: "songs#show"
+  # get '/songs', to: "songs#index"
+  # get "/songs/:id", to: "songs#show"
+  resources :songs, only: [:index, :show]
+
+
+
   get "/artists", to: "artists#index"
   get "/artists/new", to: "artists#new"
   post "/artists", to: "artists#create"
